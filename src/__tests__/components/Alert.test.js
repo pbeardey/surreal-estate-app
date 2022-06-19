d4ce700de123d7a892351509a1ff34eb6bf276e3
Paul Beard <pbeardey@gmail.com>
2022-06-19 19:15:16 +0100

    Alert component testing

---
+++ src/__tests__/components/Alert.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Alert from "../../components/Alert";
+
+describe("Alert", () => {
+  it("displays an error message", () => {
+    const { getByText, asFragment } = render(<Alert message="Error!" />);
+
+    expect(getByText(/Error/).textContent).toBe("Error!");
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("displays a success message", () => {
+    const { getByText, asFragment } = render(<Alert message="Success!!" success />);
+
+    expect(getByText(/Success/).textContent).toBe("Success!!");
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("does not render an error or a success message if message props are empty", () => {
+    const { asFragment } = render(<Alert message="" />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
