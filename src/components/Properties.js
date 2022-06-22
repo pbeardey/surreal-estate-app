b340c6a9405b55f6580bfec7f2beaf3cdddf5035
Paul Beard <pbeardey@gmail.com>
2022-06-22 21:29:55 +0100

    use axios to get properties and render data in PropertyCard; basic styling"

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,19 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import PropertyCard from "./PropertyCard";
+import Alert from "./Alert";
+import "../styles/Properties.css";
 
 const Properties = () => {
-  const data = {
-    title: "2 bedroom period property",
-    type: "Flat",
-    bedrooms: 3,
-    bathrooms: 2,
-    price: 1000,
-    city: "Manchester",
-    email: "ourHouse@street.com",
-  };
+  const API_URL = "http://localhost:3000/api/v1/PropertyListing/";
+  const [properties, setProperties] = useState([]);
+  const [alert, setAlert] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setProperties(response.data);
+        setAlert("");
+      })
+      .catch((err) => {
+        const { status } = err.response;
+        if (status === 404) {
+          setAlert("Properties not found");
+        }
+        if (status === 500) {
+          setAlert("Server Error...Please try again later.");
+        }
+      });
+  }, []);
+
   return (
-    <div>
-      <PropertyCard propertyData={data} />
+    <div className="properties-container">
+      <Alert message={alert} />
+      {properties.map((property) => (
+        <PropertyCard key={property._id} propertyData={property} />
+      ))}
     </div>
   );
 };
